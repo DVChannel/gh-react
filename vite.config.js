4e42e6f1c62e01e01314c0a617e5f977fef07a34
DVChannel <channelgonzalezjimenez100@gmail.com>
2022-08-07 13:00:39 -0500

    bug fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://DVChannel.github.io/',
+  base: 'https://DVChannel.github.io/gh-react/',
   plugins: [react()]
 })
